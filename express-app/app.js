b896ac83c47102b902a650fa01a7e60263a2b7a3
Bera Dhaval <dhaval.bera@ethicsinfotech.in>
2025-07-19 16:44:41 +0530

    test pipline

--- express-app/app.js
+++ express-app/app.js
@@ -3,7 +3,7 @@ const app = express();
 const port = 3000;
 
 app.get('/api/hello', (req, res) => {
-  res.json({ message: 'Hello from Express jenkins is working tets!' });
+  res.json({ message: 'Hello from Express jenkins is working tets2!' });
 });
 
 app.listen(port, '0.0.0.0', () => {
